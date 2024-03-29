e90e74e3834d2635f51ea364cae80422d024b269
Solomon-03 <solomon172003@gmail.com>
2024-03-29 16:00:27 +0530

    style improvement in cards

--- src/Component/PostCard/PostCard.jsx
+++ src/Component/PostCard/PostCard.jsx
@@ -1,24 +1,36 @@
+import styles from "@/app/globals.css";
+import React, { Suspense } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-
-const PostCard = ({post}) => {
+const PostCard = ({ post }) => {
   return (
-    <div className='flex flex-col'>
-      <div className='flex'>
-        <div className=' relative w-full h-[400px] z-[-1] object-cover'>
-          <Image src='/post1.jpg' alt='' fill/>
+    <div className="flex flex-col p-2 ">
+      <div className="flex bg-white rounded-lg">
+        <div className=" relative w-[300px] h-[300px] z-[1] object-cover">
+          <Suspense
+            fallback={
+              <div className="flex self-center">
+                <div className={styles.loader} />
+              </div>
+            }
+          >
+            <Image src="/post1.jpg" alt="" fill className=" rounded-tl-lg rounded-bl-lg" />
+          </Suspense>
         </div>
-        <span className='-rotate-90 m-auto text-[12px] text-nowrap'>01 july 2003</span>
+        <span className="-rotate-90 m-auto text-[12px] text-nowrap text-[#6C63FF] font-semibold">
+          01 july 2003
+        </span>
       </div>
-      <div className='p-2 flex flex-col gap-2'>
-        <h1 className='text-2xl text-[#6C63FF] font-bold'>{post.title}</h1>
-        <p className='w-[90%] text-sm'>{post.body}</p>
-        <Link href={`/blog/${post.id}`} className='underline text-[#a19df1]'>Read More..</Link>
+      <div className="p-2 flex flex-col gap-2">
+        <h1 className="text-2xl text-[#6C63FF] group-hover:text-black font-bold">{post.title}</h1>
+        <p className="w-[90%] text-sm text-justify">{post.body}</p>
+        <Link href={`/blog/${post.id}`} className="underline text-[#a19df1]">
+          Read More..
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default PostCard;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -16,7 +16,7 @@ const Blog = async () => {
   return (
     <div className='flex flex-wrap justify-center items-center gap-8 max-w-6xl mx-auto p-3 '>
       {posts.map(post=>(
-      <div className='w-[300px] h-[670px]' key={post.id}>
+      <div className='w-[350px] h-[600px] hover:bg-[#6C63FF] rounded-lg transition-all duration-700 ease-in-out group' key={post.id}>
         <PostCard post={post}/>
       </div>
       ))}
